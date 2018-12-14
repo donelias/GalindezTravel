fd077a136e6d4e525440da21936bc1b921c96ffe
Hector Galindez <hectorgalindez02@gmail.com>
2018-12-14 12:45:14 -0400

    Fix 12  My MYSQL_USER instead of MYSQL_USERNAME

--- app/User.php
+++ app/User.php
@@ -3,13 +3,12 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Caffeinated\Shinobi\Traits\ShinobiTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable, ShinobiTrait;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
